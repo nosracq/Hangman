e5f7d78d3b97d4a676e7ed2cbdea6a5585dbe8fb
nosracq <carson_14@outlook.com>
2017-08-27 11:37:43 -0700

    refactored code

--- src/components/Board.js
+++ src/components/Board.js
@@ -1,12 +1,24 @@
 import React, { Component } from 'react';
 import Dash from './Dash.js';
 
-class Board extends Component {
+//TODO: I don't like how this class is implemented currently
+//  Need to change so setting the state doesn't depend on
+//  the parent calss, Game. Should the board own the progress
+//  state or the Game. If the game owns it then it shouldn't
+//  be part of the Board state.
 
+// Maybe the Game should just evaluate the guess compared
+// to the solution and pass it back to the board if it is correct
+// so the board can update the progress.
+class Board extends Component {
   constructor(props) {
     super(props);
+    //console.log("Board constructor called");
+    // The board in each game only has one solution so this will
+    // not change after the board is first created
+    let solutionLength = this.props.solution.length;
     this.state = {
-      progress: Array(this.props.solution.length).fill(null),
+      progress: Array(solutionLength).fill(null),
     }
   }
 

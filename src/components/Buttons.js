35ca1ac7c04206218192ba3887191ef3d4156b54
nosracq <carson_14@outlook.com>
2017-08-28 21:16:08 -0700

    created Buttons component

---
+++ src/components/Buttons.js
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const Buttons = () => {
+  //render() {
+    return(
+      <div>
+        <button className='buttons' onClick=''>New Game</button>
+      </div>
+    )
+  //}
+}
+
+export default Buttons

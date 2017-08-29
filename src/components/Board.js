0f2a4b81c8067b2982c87bccfba81c32f25ef556
nosracq <carson_14@outlook.com>
2017-08-28 21:19:52 -0700

    add Fetching a word...

--- src/components/Board.js
+++ src/components/Board.js
@@ -4,10 +4,18 @@ import Dash from './Dash.js';
 // responsible for renderting the Dashes based on the progress of the Game
 const Board = (props) => {
 
+  let content;
+
+  if (!props.solution) {
+    content = <h3>Fetching a word...</h3>;
+  }
+  else {
+    content = props.solution.map((letter, index) => {
+      return <Dash key={`letter-${index}`} value={props.progress[index]}/>});
+  }
   return (
     <div className="board">
-      {props.solution.map((letter, index) => {
-        return <Dash key={`letter-${index}`} value={props.progress[index]}/>})}
+      {content}
     </div>
   )
 }
